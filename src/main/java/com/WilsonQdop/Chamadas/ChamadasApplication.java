f132dcfa7f4a813046c17584ef5974d61cca8c80
WilsonQdop <wilson.qdop@gmail.com>
2025-07-31 23:11:57 -0300

    chore: adicionar últimas pastas de configuração

---
+++ src/main/java/com/WilsonQdop/Chamadas/ChamadasApplication.java
@@ -0,0 +1,13 @@
+package com.WilsonQdop.Chamadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadasApplication.class, args);
+	}
+
+}
